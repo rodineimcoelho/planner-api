6b7b8b49e93bc104f88287b8058ce4e029a841d1
Rodinei Martins Coelho <rodinei.m.coelho@gmail.com>
2023-02-13 14:11:57 -0400

    fix: fix use of wrong port

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ dotenv.config({ path: `${__dirname}/../.env` });
 
 const port = process.env.PORT || 80;
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Server running and listening to port ${port}`);
 });
